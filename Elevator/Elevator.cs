af4c0fea61377939faf85f44d7c3cdb062964de6
StrorinWinder <StrorinWinder@DESKTOP-N2V8IFP>
2017-12-05 19:22:03 +0300

    Starting Implementing of the class

---
+++ Elevator/Elevator.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        None,
+        Opening
+    }
+
+    public class Elevator
+    {
+        private int _n;
+
+        public int N
+        {
+            get { return _n; }
+            private set { _n = value; }
+        }
+
+        private double Height;
+        private double Speed;
+        private double OpenTime;
+        private Direction MoveDirection = Direction.None;
+        private int CurrentFloor;
+        private DateTime TimeFromTheLastFloor;
+        private TimeSpan FloorChangingTime;
+
+        private List<int> _targetFloorsUp = new List<int>();
+        private List<int> _targetFloorsDown = new List<int>();
+
+        public void AddTargetFloor(int f)
+        {
+            if (f<5 | f>N)
+            {
+                throw new ArgumentException("Неверный номер этажа");
+            }
+            switch (f.CompareTo(CurrentFloor))
+            {
+                case -1:
+                    if (_targetFloorsDown.Contains(f))
+                        break;
+
+                    _targetFloorsDown.Add(f);
+                    _targetFloorsDown.Sort();
+                    _targetFloorsDown.Reverse();
+                    break;
+                case 0:
+                    break;
+                case 1:
+                    if (_targetFloorsUp.Contains(f))
+                        break;
+
+                    _targetFloorsUp.Add(f);
+                    _targetFloorsUp.Sort();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Move()
+        {
+            if (MoveDirection == Direction.None)
+            {
+                if (_targetFloorsDown.Count != 0 & _targetFloorsDown.Count >= _targetFloorsUp.Count)
+                {
+                    MoveDirection = Direction.Down;
+                    TimeFromTheLastFloor = DateTime.Now;
+                }
+                else if (_targetFloorsUp.Count != 0 & _targetFloorsUp.Count >= _targetFloorsDown.Count)
+                {
+                    MoveDirection = Direction.Up;
+                    TimeFromTheLastFloor = DateTime.Now;
+                }
+                else
+                {
+                    return; //Если мы так и не установили направление
+                }
+            }
+            if (DateTime.Now - TimeFromTheLastFloor >= FloorChangingTime)
+            {
+                
+            }
+        }
+
+        public Elevator(int n, double h, double s, double t)
+        {
+            if (n<5 | n>20)
+            {
+                throw new ArgumentException("Количество этажей может быть от 5 до 20.");
+            }
+            N = n;
+            Height = h;
+            Speed = s;
+            OpenTime = t;
+            CurrentFloor = 1;
+            var c = Height / Speed * 1000;
+            FloorChangingTime = new TimeSpan(0, 0, 0, 0, (int)c);
+        }
+    }
+}
--- Elevator/Program.cs
+++ Elevator/Program.cs
@@ -8,8 +8,76 @@ namespace Elevator
 {
     class Program
     {
+        private Elevator el;
+
         static void Main(string[] args)
         {
+            var p = new Program();
+            p.el = p.InputStartData();
+            p.PrintInstructions();
+            p.Loop();
+        }
+
+        private Elevator InputStartData()
+        {
+            Console.Write("Введите кол-во этажей в подъезде (от 5 до 20): ");
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n) | n < 5 | n > 20)
+            {
+                Console.Write("Что-то пошло не так. Введите кол-во этажей в подъезде (от 5 до 20): ");
+            }
+
+            Console.Write("Введите высоту одного этажа (в метрах): ");
+            double h;
+            while (!double.TryParse(Console.ReadLine(), out h) | h <= 0)
+            {
+                Console.Write("Что-то пошло не так. Введите высоту одного этажа (в метрах): ");
+            }
+
+            Console.Write("Введите скорость движения лифта (в метрах в секунду): ");
+            double s;
+            while (!double.TryParse(Console.ReadLine(), out s) | s <= 0)
+            {
+                Console.Write("Что-то пошло не так. Введите скорость движения лифта (в метрах в секунду): ");
+            }
+
+            Console.Write("Введите время между открытием и закрытием дверей (в секундах): ");
+            double t;
+            while (!double.TryParse(Console.ReadLine(), out t) | t <= 0)
+            {
+                Console.Write("Что-то пошло не так. Введите время между открытием и закрытием дверей (в секундах): ");
+            }
+
+            return new Elevator(n, h, s, t);
+        }
+
+        private void PrintInstructions()
+        {
+            Console.WriteLine();
+            Console.WriteLine(@"В течение выполнения программы вы можете печатать числа, это будут " +
+                   "нажатия на кнопки лифта на этажах или внутри кабины. Ввод должен быть завершен нажатием " +
+                   "клавиши 'Ввод'.");
+        }
+
+        private void Loop()
+        {
+            var t = Console.In.ReadLineAsync();
+            while (true)
+            {
+                if (t.IsCompleted)
+                {
+                    int num;
+                    if (!int.TryParse(t.Result, out num) | num<5 | num > el.N)
+                    {
+                        Console.WriteLine("Некорректный формат ввода.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(t.Result);
+                    }       
+                    t = Console.In.ReadLineAsync();
+                }
+            }
         }
     }
 }
